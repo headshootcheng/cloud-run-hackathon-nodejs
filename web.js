548cef4504f13e2b3ca17f716f3fc1ee8cf7e2a3
petercheng <petercheng7788@gmail.com>
2022-05-27 12:38:08 +0800

    updated logic

--- web.js
+++ web.js
@@ -37,11 +37,42 @@ app.post('/', function (req, res) {
   const width = arena?.dims[0]?? 12;
   const height = arena?.dims[1]?? 9;
   const myLink =  _links?.self?.href ?? "https://cloud-run-hackathon-nodejs-h4njltnl3q-uc.a.run.app/";
+  const myPosition = {x: arena.state[myLink].x, y: arena.state[myLink].y};
   const myDirection = arena.state[myLink].direction;
+  const allUserPosition = Object.values(arena.state).map((player)=>({
+    x: player.x,
+    y: player.y
+  }))
   console.log("myDirection",myDirection);
-  const moves = ["T", 'L', "T", 'R', "T","F","T"];
+  let shouldShoot = false;
+  switch(myDirection){
+    case "N":{
+       if(allUserPosition.some((position)=>position.x===myPosition.x && position.y - myPosition.y <=3 &&   position.y > myPosition.y ))
+        shouldShoot = true;
+        break;
+    }
+    case "S":{
+      if(allUserPosition.some((position)=>position.x===myPosition.x && myPosition.y - position.y <=3 &&   position.y <  myPosition.y ))
+        shouldShoot = true;
+        break;
+    }
+    case "E": {
+      if(allUserPosition.some((position)=>position.y===myPosition.y &&  position.x - myPosition.x   <=3 &&   position.x >  myPosition.x ))
+        shouldShoot = true;
+        break;
+    }
+    case "W": {
+      if(allUserPosition.some((position)=>position.y === myPosition.y &&  myPosition.x - position.x <=3 && position.x <  myPosition.x ))
+        shouldShoot = true;
+        break;
+    }
+    default:
+      shouldShoot = true;
+  }
+  const moves = ["R","L","F"];
+  // const moves = ["T", 'L', "T", 'R', "T","F","T"];
   const randomNum = Math.floor(Math.random())*moves.length;
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send(shootShoot? "T" : moves[Math.floor(Math.random() * moves.length)]);
 });
 
 app.listen(process.env.PORT || 8080);
